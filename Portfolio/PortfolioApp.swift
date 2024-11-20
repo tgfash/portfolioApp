e9b2baa7da818aa2dd01c780de4db9290b8a9104
Nguyen Ngoc Tan <tannn@deha-soft.com>
2024-11-20 13:41:30 +0700

    init app

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by User on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
